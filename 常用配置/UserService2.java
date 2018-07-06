a6e3d1226c29226b873bdf3696f6fd20b080ab2d
zjg <zjg@qq.com>
2018-07-06 12:39:22 +0800

    0..

---
+++ 常用配置/UserService2.java
@@ -0,0 +1,26 @@
+package com.atguigu.mapper.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.atguigu.mapper.entities.User;
+import com.atguigu.mapper.mappers.UserMapper2;
+
+@Service
+public class UserService2 {
+	
+	@Autowired
+	UserMapper2 userMapper2;
+	
+	public User selectUser(int sid)
+	{
+		return userMapper2.selectUserOneById(sid);
+	}
+	
+	
+}
