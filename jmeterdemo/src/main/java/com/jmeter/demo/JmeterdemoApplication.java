a95d2b4005e151c19bc62e4f7823a8b2862533d4
zjg <zjg@qq.com>
2018-12-09 18:44:45 +0800

    springcloud timeout

--- jmeterdemo/src/main/java/com/jmeter/demo/JmeterdemoApplication.java
+++ jmeterdemo/src/main/java/com/jmeter/demo/JmeterdemoApplication.java
@@ -3,10 +3,13 @@ package com.jmeter.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan(basePackages = "com.jmeter.demo.dao")
 @SpringBootApplication
+@RestController
 public class JmeterdemoApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +17,16 @@ public class JmeterdemoApplication {
 	}
 
 
+	@RequestMapping("/")
+	public String home()
+	{
+		return "spring boot security demo home.....";
+	}
+
+	@GetMapping("/hello")
+    public String hello()
+    {
+        return "hello....";
+    }
 
 }
